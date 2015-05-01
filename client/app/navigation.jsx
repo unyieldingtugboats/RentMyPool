106e91f6ae9fca6f07f839f18488f2d8eb39538a
Hack Reactor Students <accounts@hackreactor.com>
2015-04-30 22:24:31 -0700

    Refactor Navigation with state

--- client/app/navigation.jsx
+++ client/app/navigation.jsx
@@ -1,11 +1,39 @@
 var createNav = function () {
   var Navigation = React.createClass({
+
+    getInitialState: function(){
+        return { focused: 0 };
+    },
+
+    clicked: function(index, cb){
+
+        // The click handler will update the state with
+        // the index of the focused menu entry
+
+        this.setState({focused: index});
+        cb();
+    },
+
     render: function () {
+
+      var self = this;
       return (
         <div className="nav-menu">
-          <a className="nav-link" href="javascript:renderHome()">Home</a>
-          <a className="nav-link" href="javascript:renderRent()">Rent a Pool</a>
-          <a className="nav-link" href="javascript:renderList()">List a Pool</a>
+          { this.props.items.map(function(m, index){
+
+                    var style = 'nav-link';
+
+                    if(self.state.focused == index){
+                        style = 'nav-link-focused';
+                    }
+
+                    // Notice the use of the bind() method. It makes the
+                    // index available to the clicked function:
+
+                    return <a className={style} onClick={self.clicked.bind(self, index, m.cb)}>{m.name}</a>;
+
+          }) }
+
         </div>
       );
     }
@@ -26,7 +54,11 @@ var createNav = function () {
       return (
         <div className="viewPort">
 
-          <Navigation />
+          <Navigation items={ [
+            {name:'Home',cb:renderHome},
+            {name:'Rent',cb:renderRent},
+            {name:'List',cb:renderList}
+          ] } />
           <div className="main">
           </div>
         </div>
--- client/res/readme.txt
+++ client/res/readme.txt
@@ -1,1 +1,6 @@
 This folder is for reasources
+
+http://jsfiddle.net/martinaglv/sY6nX/light/
+http://tutorialzine.com/2014/07/5-practical-examples-for-learning-facebooks-react-framework/
+
+https://zapier.com/engineering/react-js-tutorial-guide-gotchas/
