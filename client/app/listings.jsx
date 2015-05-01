15cceaac6ba6d225813c7ba8d778bc6ca7eebcd7
Hack Reactor Students <accounts@hackreactor.com>
2015-05-01 15:57:00 -0700

    input fields

--- client/app/listings.jsx
+++ client/app/listings.jsx
@@ -1,17 +1,32 @@
 var renderList = function () {
   var ListContent = React.createClass({
+
+    getInitialState: function() {
+    return {
+      name: 'Name',
+      address : 'Address',
+      price : 'Price'
+      };
+    },
+
+    handleChange: function(event) {
+      var state = {};
+      state[event.target.name] = event.target.value;
+      this.setState(state);
+    },
+
     render: function () {
       return (
         <div className="listView">
           <h1>List a Pool</h1>
           <form method="POST" action="/list">
-            <input name="name" value="Name" type="text" />
+            <input name="name" value={this.state.name} onChange={this.handleChange}  type="text" />
             <br />
             <br />
-            <input name="address" value="Address" type="text" />
+            <input name="address" value={this.state.address} onChange={this.handleChange}  type="text" />
             <br />
             <br />
-            <input name="price" value="Price" type="text" />
+            <input name="price" value={this.state.price} onChange={this.handleChange}  type="text" />
             <br />
             <br />
             <input type="submit" />
--- client/app/upload.jsx
+++ client/app/upload.jsx
@@ -1,3 +1,25 @@
+var uploadImg = function () {
+  $('#uploadForm').submit(function() {
+        //status('uploading the file ...');
+
+        $(this).ajaxSubmit({
+
+            error: function(xhr) {
+              status('Error: ' + xhr.status);
+            },
+
+            success: function(response) {
+    //TODO: We will fill this in later
+            }
+        });
+
+  // Have to stop the form from submitting and causing
+  // a page refresh - don't forget this
+    return false;
+  });
+
+}
+
 var renderImgUpload = function () {
   var ImgUploadContent = React.createClass({
     render: function () {
@@ -6,13 +28,14 @@ var renderImgUpload = function () {
           <h1>Upload Image</h1>
           <form id="uploadForm"
             enctype="multipart/form-data"
-            action="/api/photos"
+            action="/uploadimg"
             method="post">
             <input type="file" id="userPhotoInput" name="userPhoto" />
           </form>
 
         <span id="status" />
         <img id="uploadedImage" />
+        <button type="button" onClick={uploadImg}>Upload to Server</button>
       </div>
       );
     }
