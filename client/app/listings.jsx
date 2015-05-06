a5f6994e1aeca7a6be997255e5b7f40d475ecdc2
John Pizzo <jjpizzo4@gmail.com>
2015-05-06 15:11:49 -0700

    Refactored to use better input practices

--- client/app/listings.jsx
+++ client/app/listings.jsx
@@ -8,27 +8,30 @@ var ListContent = React.createClass({
     };
   },
 
-  handleChange: function(event) {
-    var state = {};
-    state[event.target.name] = event.target.value;
-    this.setState(state);
-  },
-
   handleSubmit: function (e) {
     e.preventDefault();
 
-    $.ajax({
-      url: "/list",
-      method: "POST",
-      contentType: "application/json",
-      data: JSON.stringify(this.state),
-      success: function (){
-        console.log("POST Successful.");
-      },
-      error: function (err) {
-        console.log("Error:", err)
-      }
+    this.setState({
+      name: e.target.name.value,
+      address: e.target.address.value,
+      price: e.target.price.value
+    }, function () {  
+        console.log(this.state)
+
+        $.ajax({
+          url: "/list",
+          method: "POST",
+          contentType: "application/json",
+          data: JSON.stringify(this.state),
+          success: function (){
+            console.log("POST Successful.");
+          },
+          error: function (err) {
+            console.log("Error:", err)
+          }
+        });
     });
+
   },
 
   render: function () {
@@ -36,13 +39,13 @@ var ListContent = React.createClass({
       <div className="listView">
         <h1>List a Pool</h1>
         <form onSubmit={this.handleSubmit}>
-          <input name="name" value={this.state.name} onChange={this.handleChange}  type="text" />
+          <input name="name" placeholder={this.state.name} type="text" />
           <br />
           <br />
-          <input name="address" value={this.state.address} onChange={this.handleChange}  type="text" />
+          <input name="address" placeholder={this.state.address} type="text" />
           <br />
           <br />
-          <input name="price" value={this.state.price} onChange={this.handleChange}  type="text" />
+          <input name="price" placeholder={this.state.price} type="text" />
           <br />
           <br />
           <input type="submit" />
