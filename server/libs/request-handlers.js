c2fad1f839eeb700e8c28a46bb423381994c5841
John Pizzo <jjpizzo4@gmail.com>
2015-05-11 18:16:39 -0700

    Redirects

--- server/libs/request-handlers.js
+++ server/libs/request-handlers.js
@@ -152,7 +152,7 @@ exports.signUpUser = function(req, res) {
           res.status(500).send({errorMessage: 'error in saving user info to Database'});
         } 
         else {
-          utils.createSession(req,res,user);
+          res.status(302).send("Login");
         }
       });
     }
